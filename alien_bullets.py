5fc7d0a0b1ff8d27c909b25bbcaf185c31cacc51
henrymo1204 <48542636+henrymo1204@users.noreply.github.com>
2021-02-24 01:14:21 -0800

    update

---
+++ alien_bullets.py
@@ -0,0 +1,70 @@
+import pygame as pg
+from pygame.sprite import Sprite
+from random import randint
+import time
+
+
+class AlienBullets:
+    def __init__(self, bullet_group, enemy_group, enemy_bullet_group, ship_group, settings, stats, sb, game):
+        self.bullets = bullet_group
+        self.human_group = enemy_group
+        self.human_bullet_group = enemy_bullet_group
+        self.ship_group = ship_group
+        self.settings = settings
+        self.stats = stats
+        self.sb = sb
+        self.game = game
+
+    def add(self, settings, screen, ship):
+        self.bullets.add(Bullet(settings=settings, screen=screen, ship=ship))
+
+    def update(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom >= self.settings.screen_height:
+                self.bullets.remove(bullet)
+            elif bullet.gone:
+                self.bullets.remove(bullet)
+        if pg.sprite.groupcollide(self.bullets, self.ship_group, True, False):
+            if not self.game.ship.gone:
+                self.game.ship.hit = True
+        if self.game.ship.gone:
+            self.game.ship.gone = False
+            self.bullets.empty()
+            self.human_bullet_group.empty()
+            self.game.reset()
+            return
+        if pg.sprite.groupcollide(self.bullets, self.human_group, True, False):
+            print('hit')
+        # if len(self.alien_group) == 0:
+        #     self.bullets.empty()
+        #     self.settings.increase_speed()
+        #     self.aliens.create_fleet()
+        #     self.stats.level += 1
+        #     self.sb.prep_level()
+
+    def draw(self):
+        for bullet in self.bullets.sprites():
+            bullet.draw()
+
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship"""
+    def __init__(self, settings, screen, ship):
+        super().__init__()
+        self.screen = screen
+        self.rect = pg.Rect(0, 0, settings.bullet_width, settings.bullet_height)
+        self.rect.centerx = ship.rect.centerx
+        self.rect.bottom = ship.rect.bottom
+        self.y = float(self.rect.y)
+        # self.color = settings.bullet_color
+        self.color = (255, 0, 0)
+        self.speed_factor = settings.bullet_speed_factor
+        self.gone = False
+
+    def update(self):
+        self.y += self.speed_factor
+        self.rect.y = self.y
+
+    def draw(self):
+        pg.draw.rect(self.screen, self.color, self.rect)
---
+++ barrier.py
@@ -0,0 +1,68 @@
+from pygame.sprite import Sprite
+import pygame
+from timer import Timer
+
+
+class Barriers:
+    def __init__(self, settings, screen, ally_group, ship_height, game):
+        self.settings = settings
+        self.allies = ally_group
+        self.screen = screen
+        self.game = game
+        self.ship_height = ship_height
+        self.create_barriers()
+
+    def create_barriers(self):
+        settings, screen = self.settings, self.screen
+        barriers = Barrier(settings=settings, screen=self.screen)
+        alien_width = barriers.rect.width
+        alien_height = barriers.rect.height
+
+        for i in range(3):
+            barrier = Barrier(settings=settings, screen=screen, x=alien_width * (1.205 + 3 * i),
+                                  y=alien_height * 4.6)
+            self.allies.add(barrier)
+
+    def draw(self):
+        for ally in self.allies.sprites(): ally.draw()
+
+
+class Barrier(Sprite):
+    def __init__(self, settings, screen, x=0, y=0):
+        """Initialize the ship and set its starting position."""
+        super().__init__()
+        self.screen = screen
+        self.settings = settings
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('images/temp2.png')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        self.rect.x = self.x = x
+        self.rect.y = self.y = y
+
+    def draw(self):
+        # image = Alien.images[self.number]
+        # self.screen.blit(image, self.rect)
+        image = self.image
+        rect = image.get_rect()
+        rect.x, rect.y = self.rect.x, self.rect.y
+        self.screen.blit(image, rect)
+        # self.screen.blit(self.image, self.rect)
+
+    # def update(self):
+    #     delta = self.settings.ship_speed_factor
+    #     if self.moving_right and self.rect.right < self.screen_rect.right: self.center += delta
+    #     if self.moving_left  and self.rect.left > 0: self.center -= delta
+    #     if self.shooting_bullets:
+    #         self.sound.shoot_bullet()
+    #         self.bullets.add(settings=self.settings, screen=self.screen, ship=self)
+    #         self.shooting_bullets = False
+    #         # self.bullets_attempted += 1
+    #         # if self.bullets_attempted % self.settings.bullets_every == 0:
+    #         #     self.sound.shoot_bullet()
+    #         #     self.bullets.add(settings=self.settings, screen=self.screen, ship=self)
+    #     self.rect.centerx = self.center
+
+    # def draw(self): self.screen.blit(self.image, self.rect)
